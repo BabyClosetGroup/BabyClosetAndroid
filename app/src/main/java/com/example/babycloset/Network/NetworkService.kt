492be97220f4ba6197bc38d6db718a3c1ac395b2
godjoy <tlschgml556@gmail.com>
2019-08-31 01:28:39 +0900

    .

---
+++ app/src/main/java/com/example/babycloset/Network/NetworkService.kt
@@ -0,0 +1,4 @@
+package com.example.babycloset.Network
+
+interface NetworkService {
+}
---
+++ app/src/main/java/com/example/babycloset/UI/Adapter/BlankActivity.kt
@@ -0,0 +1,12 @@
+package com.example.babycloset.UI.Adapter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BlankActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blank)
+    }
+}
