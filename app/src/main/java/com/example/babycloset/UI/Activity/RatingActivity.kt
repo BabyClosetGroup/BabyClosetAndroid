df3579a947ab5ce9bc9cb8614069fbd63b4298e0
zziyun <jiyun7670@naver.com>
2019-09-01 10:58:53 +0900

    >> mypage xml

---
+++ app/src/main/java/com/example/babycloset/UI/Activity/RatingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.babycloset.UI.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.babycloset.R
+
+class RatingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+    }
+}
---
+++ app/src/main/java/com/example/babycloset/UI/Activity/ReceiveProductActivity.kt
@@ -0,0 +1,13 @@
+package com.example.babycloset.UI.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.babycloset.R
+
+class ReceiveProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receive_product)
+    }
+}
