eeba494324d46349c57cf675b20090d5a2237fbf
zziyun <jiyun7670@naver.com>
2019-09-09 00:14:39 +0900

    >> 정보수정

--- app/src/main/java/com/example/babycloset/UI/Activity/EditInfoActivity.kt
+++ app/src/main/java/com/example/babycloset/UI/Activity/EditInfoActivity.kt
@@ -1,26 +1,36 @@
 package com.example.babycloset.UI.Activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.babycloset.R
 import kotlinx.android.synthetic.main.activity_edit_info.*
+import kotlinx.android.synthetic.main.toolbar_edit_info.*
 
 class EditInfoActivity : AppCompatActivity() {
+
+    val REQUEST_CODE_SELECT_IMAGE: Int = 1004
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_info)
 
-        //프로필사진 원형 출력
-        //통신으로 닉네임, 이름, 아이디, 비밀번호 가져오기?
-        //비밀변호변경 어떤식으로..??
-        //아이디 이름은 출력만?
-        //닉네임 수정기능
-        //사진 수정기능(갤러리연동)
-        //체크버튼 누를시 저장되고 마이페이지로 이동
+        btn_save_info.setOnClickListener {
+            // 데이터 저장
+            finish()
+        }
+
+        btn_pw_del.setOnClickListener {
+            // x클릭시 비번칸 지우기
+            txt_info_pw.text=null
+        }
 
         img_info_thumbnail.setOnClickListener {
             //갤러리 연동
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+            intent.data = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE)
         }
     }
 }
