709e2ea1edc016996785cd8a82e4844f91a9de48
hyesun <nbn0809@naver.com>
2019-09-09 21:03:14 +0900

    qr 완료

--- app/src/main/java/com/example/babycloset/UI/Activity/QRMainActivity.kt
+++ app/src/main/java/com/example/babycloset/UI/Activity/QRMainActivity.kt
@@ -3,11 +3,17 @@ package com.example.babycloset.UI.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.babycloset.R
+import kotlinx.android.synthetic.main.activity_qrmain.*
+import org.jetbrains.anko.startActivity
 
 class QRMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrmain)
+
+        btn_qr_scan.setOnClickListener {
+            startActivity<QRScanActivity>("id" to 1)
+        }
     }
 }
